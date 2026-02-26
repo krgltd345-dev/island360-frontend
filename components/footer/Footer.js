ae4e5af97cd92768068cb2d38a8c7f1a632ea87c
yakshaptyagi-czlabs <yakshap@czlabs.io>
2026-02-26 17:28:41 +0530

    privacy and t&c page

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,20 +1,37 @@
 import React from 'react'
+import Link from 'next/link'
+import { Separator } from '../ui/separator'
 
 const Footer = () => {
   return (
     <footer className="bg-white border-t border-slate-100 py-8 mt-auto relative">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
-              <div className="flex items-center gap-2 text-slate-600">
-                <img src={"/island_logo.png"} alt="Logo" className="w-8 h-6" />
-                <span className="font-semibold">Island 360</span>
-              </div>
-              <p className="text-sm text-slate-500">
-                © {new Date().getFullYear()} Island 360. All rights reserved.
-              </p>
-            </div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row items-center justify-between gap-4">
+          <div className="flex lg:w-60 items-center gap-2 text-slate-600">
+            <img src={"/island_logo.png"} alt="Logo" className="w-8 h-6" />
+            <span className="font-semibold">Island 360</span>
+          </div>
+          <div className='flex text-sm items-center text-slate-500 gap-1'>
+            <Link
+              href="/terms-of-service"
+              className="underline underline-offset-2 hover:text-slate-700"
+            >
+              Terms of Service
+            </Link>
+            |
+            <Link
+              href="/privacy-policy"
+              className="underline underline-offset-2 hover:text-slate-700"
+            >
+              Privacy Policy
+            </Link>
+          </div>
+          <p className="text-sm text-slate-500">
+            © {new Date().getFullYear()} Island 360. All rights reserved.{" "}
+          </p>
         </div>
-      </footer>
+      </div>
+    </footer>
   )
 }
 
--- middleware.js
+++ middleware.js
@@ -100,7 +100,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|island_logo.png|icons|fonts|cover1.jpeg).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|island_logo.png|default.png|icons|fonts|cover1.jpeg).*)',
   ],
 };
 
